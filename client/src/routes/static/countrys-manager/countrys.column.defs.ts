b935fb4efdc9649e85dde87f48f60dd4874c3dd2
jay-taylerson <87064668+jay-taylerson@users.noreply.github.com>
2024-09-06 11:24:00 +0100

    Sync to genesiscommunity

---
+++ client/src/routes/static/countrys-manager/countrys.column.defs.ts
@@ -0,0 +1,8 @@
+import { ColDef } from '@ag-grid-community/core';
+import { getDateFormatter, getNumberFormatter } from '../../../utils';
+
+export const columnDefs: ColDef[] = [
+  {
+    field: "COUNTRY_NAME",
+  }
+]
